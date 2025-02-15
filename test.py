9bd680df66912f5956cb747ed20fa30ea4152ff1
Roman <stepanov_roman777666@mail.ru>
2025-02-15 09:49:32 +0300

    Initial commit with environment and test script

---
+++ test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib
+import numpy as np
+import os
+
+print('\n\n\nIf you see this message, its OK. Congratulations!')
+print('\nCurrent environment is', os.environ.get('CONDA_DEFAULT_ENV', 'Unknown'), '\n\n\n')
